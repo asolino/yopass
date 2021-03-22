af56592ebeaa2468814f480ac9bc1ed764720adc
Alberto Solino <bethus@gmail.com>
2021-03-22 08:19:22 -0300

    Removing routes for sending decryption key
    - You need to have the decryption key yourself.

--- website/src/Routes.tsx
+++ website/src/Routes.tsx
@@ -7,9 +7,7 @@ export const Routes = () => {
   return (
     <div>
       <Route path="/" exact={true} component={CreateSecret} />
-      <Route exact={true} path="/s/:key/:password" component={DisplaySecret} />
       <Route exact={true} path="/s/:key" component={DisplaySecret} />
-      <Route exact={true} path="/f/:key/:password" component={DisplaySecret} />
       <Route exact={true} path="/f/:key" component={DisplaySecret} />
     </div>
   );
